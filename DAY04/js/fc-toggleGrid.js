9583392532ac6ecd4099ba85f0c2e115b30c6dab
Jee Hoon <yamoo9@naver.com>
2015-11-14 11:00:17 +0900

    4일차 첫시간 진행 내용

--- DAY04/js/fc-toggleGrid.js
+++ DAY04/js/fc-toggleGrid.js
@@ -7,76 +7,59 @@
  * 버튼을 그린 다음에 그 버튼을 클릭하면
  * 그리드가 토글 되는 기능 구현
 */
-
-// ------------------------------------------
-// 변수 정의
-// var user_variable, my_variable; // undefined
-
-// ------------------------------------------
-// 변수에 값을 할당(대입)
-// user_variable = 9;
-// my_variable   = true;
-
-// ------------------------------------------
-// 진보 이벤트 모델
-// W3C 표준 addEventListener, removeEventListener (IE 9+)
-// MS 비표준 attachEvent, detachEvent
-
-// 문서객체모델(DOM)이 준비되면... 실행 코드
-// window.addEventListener('DOMContentLoaded', callback);
-
-// jQuery 레디 이벤트
-// jQuery(document).ready( callback );
-// jQuery( callback );
-
-// ------------------------------------------
-// var single pattern
-// IIFE pattern
-// ------------------------------------------
 (function(){
+	'use strict';
 
-	// 1. 문서에서 토글 그리드 버튼(.toggle-grid-btn), 컨테이너(.container) 요소를 찾는다.
-	var toggle_grid_btn = document.querySelector('.toggle-grid-btn'),
+	// 탐색
+	var toggle_grid_btn = document.createElement('i'), // 생성
 		container       = document.querySelector('.container'),
 		class_name      = 'grid',
-	// console.log( toggle_grid_btn );
-	// console.log( container );
 
-		// 2. 클릭했을 때 처리할 함수(이벤트 핸들러)를 정의한다.
 		toggleGrid = function (event) {
-
 			var type = event.type,
 				key  = event.keyCode;
-
 			if (
-				// 클릭 할 때
 				type === 'click' ||
-				// 키보드 누를 때 (Enter || Space)
 				( type === 'keydown' && ( key === 13 || key === 32 ) )
 			) {
-				// 최신 DOM 방식의 Javascript 코드
 				container.classList.toggle(class_name);
-				// if ( container.classList.contains(class_name) ) {
-				// 	container.classList.remove(class_name);
-				// } else {
-				// 	container.classList.add(class_name);
-				// }
 			}
-
-			// 예전 HTML DOM 방식의 Javascript 코드
-			// console.log('clicked');
-			// if( container.className.indexOf(c_name) > -1 ) { // 'container grid'
-			// 	container.className.replace(/grid/, '');
-			// } else {
-			// 	container.className += ' ' + c_name;
-			// }
 		};
 
-	// 3. 찾은 버튼을 클릭하면 ... (클릭 이벤트 핸들링)
-	toggle_grid_btn.onclick = toggleGrid;
-	// toggle_grid_btn.addEventListener('click', toggleGrid);
+	console.log( toggle_grid_btn );
 
-	// 4. 비 포커스 요소를 사용자 정의 컴포넌트 접근성 적용을 위한 키보드 이벤트 처리
+	// 속성 추가
+	// class :: Legacy DOM (HTML DOM) -> id, title, className
+	// role
+	// aria-label
+	// tabindex
+
+	// 과거 방식으로 속성 값을 설정
+	// toggle_grid_btn.className = 'fa fa-navicon';
+
+	// 새로운 속성은 새로운 방식으로 값을 설정(SET)
+	toggle_grid_btn.setAttribute('role', 'button');
+	toggle_grid_btn.setAttribute('aria-label', '토글 그리드');
+	toggle_grid_btn.setAttribute('tabindex', 0);
+	toggle_grid_btn.setAttribute('class', 'fa fa-navicon');
+
+	// jQuery 코드
+	$('<i>')
+		.attr('role', 'button')
+		.attr('aria-label', '토글 그리드')
+		.attr('tabindex', 0)
+		.attr('class', 'fa fa-navicon')
+
+	var toggle_grid_btn_attr_map = {
+		'role'       : 'button',
+		'aria-label' : '토글 그리드',
+		'tabindex'   : 0,
+		'class'      : 'fa fa-navicon'
+	};
+
+	$('<i>').attr( toggle_grid_btn_attr_map );
+
+	toggle_grid_btn.onclick   = toggleGrid;
 	toggle_grid_btn.onkeydown = toggleGrid;
 
 }());
