1c4abc40a8d3cce82b6ffaaf9a1494a7460dcddd
Jee Hoon <yamoo9@naver.com>
2015-11-14 22:37:37 +0900

    4일차 수업 내용 정리

--- DAY04/js/fc-toggleGrid.js
+++ DAY04/js/fc-toggleGrid.js
@@ -1,97 +1,43 @@
-/*!
- * fc-toggleGrid.js © yamoo9.net, 2015
- * ------------------------------------------
- * 화면 단에 그리드를 가득 채워서 표시합니다.
- * 960px, 12컬럼, 컬럼 폭 60px, 거터 폭 10+10px
- * .container 요소는 화면 가운데 정렬
- * 버튼을 그린 다음에 그 버튼을 클릭하면
- * 그리드가 토글 되는 기능 구현
-*/
+/*! fc-toggleGrid.js © yamoo9.net, 2015 */
 (function(global, DOM){
 	'use strict';
 
-	// console.log(DOM);
+	var	class_name       = 'grid',
+		// 탐색
+		body             = document.body,
+		link_style       = document.querySelector('[href="css/style.css"]'),
+		container        = document.querySelector('.container'),
+		// 생성
+		link_fontawesome = document.createElement('link'),
+		toggle_grid_btn  = document.createElement('i'),
 
-	// 탐색
-	var toggle_grid_btn = document.createElement('i'), // 생성
-		container       = document.querySelector('.container'),
-		class_name      = 'grid',
-
+		// 토글그리드 함수 정의
 		toggleGrid = function (event) {
 			var type = event.type,
 				key  = event.keyCode;
-			if (
-				type === 'click' ||
-				( type === 'keydown' && ( key === 13 || key === 32 ) )
-			) {
+			if ( type === 'click' || type === 'keydown' && ( key === 13 || key === 32 ) ) {
 				container.classList.toggle(class_name);
 			}
 		};
 
-	// console.log( toggle_grid_btn );s
-
-	// 속성 추가
-	// class :: Legacy DOM (HTML DOM) -> id, title, className
-	// role
-	// aria-label
-	// tabindex
-
-	// 과거 방식으로 속성 값을 설정
-	// toggle_grid_btn.className = 'fa fa-navicon';
-
-
-	// 새로운 속성은 새로운 방식으로 값을 설정(SET)
-	// toggle_grid_btn.setAttribute('role', 'button');
-	// toggle_grid_btn.setAttribute('aria-label', '토글 그리드');
-	// toggle_grid_btn.setAttribute('tabindex', 0);
-	// toggle_grid_btn.setAttribute('class', 'fa fa-navicon');
+	// 속성 조작
+	DOM.attr(link_fontawesome, {
+		'rel'  : 'stylesheet',
+		'href' : 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
+	});
 
 	DOM.attr(toggle_grid_btn, {
 		'role'       : 'button',
 		'aria-label' : '토글 그리드',
 		'tabindex'   : 0,
-		'class'      : 'fa fa-navicon'
+		'class'      : 'fa fa-navicon toggle-grid-btn'
 	});
 
-	// console.log( toggle_grid_btn.outerHTML );
+	// DOM에 추가
+	DOM.before( link_style , link_fontawesome );
+	DOM.prepend( body, toggle_grid_btn );
 
-	// 문서에 추가
-	// parentElement.appendChild(childElement)
-	// childElement.parentNode.appendChild(childElement)
-	// document.body.appendChild(toggle_grid_btn);
-
-	var btn_code = toggle_grid_btn.outerHTML;
-
-	// beforebegin
-	// afterbegin
-	// beforeend
-	// afterend
-
-	// document.body.insertAdjacentHTML('afterend', btn_code);
-	console.log(DOM.prepend(container, btn_code));
-
-	// DOM.prepend(부모, 자식) | DOM.prependTo(자식, 부모)
-	// DOM.append()
-	// DOM.before()
-	// DOM.after()
-
-
-	// jQuery 코드
-	// $('<i>')
-	// 	.attr('role', 'button')
-	// 	.attr('aria-label', '토글 그리드')
-	// 	.attr('tabindex', 0)
-	// 	.attr('class', 'fa fa-navicon');
-
-	// var toggle_grid_btn_attr_map = {
-	// 	'role'       : 'button',
-	// 	'aria-label' : '토글 그리드',
-	// 	'tabindex'   : 0,
-	// 	'class'      : 'fa fa-navicon'
-	// };
-
-	// $('<i>').attr( toggle_grid_btn_attr_map );
-
+	// 이벤트 핸들러 연결
 	toggle_grid_btn.onclick   = toggleGrid;
 	toggle_grid_btn.onkeydown = toggleGrid;
 
--- DAY04/js/modules/dom.js
+++ DAY04/js/modules/dom.js
@@ -2,50 +2,169 @@
 var DOM = (function(global, undefined){
 	'use strict';
 
-	function _before(el, previousElCode) {
+	/**
+	 * ----------------------------------------------------------------
+	 * 데이터 유형 체크
+	 * ----------------------------------------------------------------
+	 */
+	var toString = Object.prototype.toString;
+
+	function _isType(data, type) {
+		return toString.call(data).slice(8, -1).toLowerCase() === type;
+	}
+
+	function _isObject(data) {
+		return _isType(data, 'object');
+	}
+
+	function _isArray(data) {
+		return _isType(data, 'array');
+	}
+
+	function _isFunction(data) {
+		return _isType(data, 'function');
+	}
+
+
+
+	/**
+	 * ----------------------------------------------------------------
+	 * DOM 조작
+	 * ----------------------------------------------------------------
+	 */
+
+	// DOM 조작(HTML 코드 삽입)
+	// ----------------------------------------------------------------
+	function _beforeHTML(el, previousElCode) {
 		el.insertAdjacentHTML('beforebegin', previousElCode);
 	}
 
-	function _after(el, nextElCode) {
+	function _afterHTML(el, nextElCode) {
 		el.insertAdjacentHTML('afterend', nextElCode);
 	}
 
-	function _append(parentEl, childElCode) {
+	function _appendHTML(parentEl, childElCode) {
 		parentEl.insertAdjacentHTML('beforeend', childElCode);
 	}
 
-	function _prepend(parentEl, childElCode) {
+	function _prependHTML(parentEl, childElCode) {
 		parentEl.insertAdjacentHTML('afterbegin', childElCode);
 	}
 
-	/**
-	 * _attr(); 함수에 전달된 객체를 탐색하여 요소에 개별적으로 적용하는 일을 수행
-	 * @param  {[DOM element]} element   DOM 객체
-	 * @param  {Object} attributes       속성 맵 객체
-	 * @return {undefined}
-	 */
-	function _attr(element, attributes) {
-		// 데이터 유형 체크 (validation)
-		if ( !element || !element.nodeName || attributes.constructor !== Object ) {
-			// console.error('전달된 데이터 유형(DOM객체, 객체)을 체크해주세요.');
-			throw {
-				'name'    : '데이터 유형 오류',
-				'message' : '전달된 데이터 유형(DOM객체, 객체)을 체크해주세요.'
-			}
-		}
+	// DOM 조작(요소 삽입)
+	// ----------------------------------------------------------------
+	function _firstChild(el) {
+		var children = el.childen;
+		return children ? children[0] : null;
+	}
 
+	function _lastChild(el) {
+		var children = el.childen;
+		return children ? children[children.length - 1] : null;
+	}
+
+	function _before(targetEl, el) {
+		targetEl.parentNode.insertBefore(el, targetEl);
+	}
+
+	function _append(targetEl, el) {
+		targetEl.appendChild(el);
+	}
+
+	function _prepend(targetEl, el) {
+		var first_child = _firstChild(targetEl);
+		targetEl.insertBefore(el, first_child);
+	}
+
+
+
+	// 객체 리터럴 맵 속성 복사
+	// ----------------------------------------------------------------
+	function _assignObjectAttributes(element, attributes) {
 		for ( var key in attributes ) {
 			var value = attributes[key];
 			element.setAttribute(key, value);
 		}
 	}
 
+	// 속성 설정
+	// ----------------------------------------------------------------
+	function _attr(element, attributes) {
+		// 데이터 유형 체크 (validation)
+		if ( !element || !element.nodeName || !_isObject(attributes) ) {
+			// console.error('전달된 데이터 유형(DOM객체, 객체)을 체크해주세요.');
+			throw {
+				'name'    : '데이터 유형 오류',
+				'message' : '전달된 데이터 유형(DOM객체, 객체)을 체크해주세요.'
+			}
+		}
+		_assignObjectAttributes(element, attributes);
+	}
+
+	// CSS 설정
+	// ----------------------------------------------------------------
+	var _getStyle = (function(){
+		var __getStyle;
+		if ( global.getComputedStyle ) {
+			__getStyle = function (el, prop) {
+				return global.getComputedStyle(el, null)[prop];
+			}
+		} else {
+			__getStyle = function (el, prop) {
+				return el.currentStyle[prop];
+			}
+		}
+		return __getStyle;
+	})();
+
+	function _setStyle(el, prop, value) {
+		el.style[prop] = value;
+	}
+
+	// 팩토리 패턴 (어떤 유형을 전달하는가에 따라서 각기 달리 처리)
+	function _css(el, prop, value) {
+		// CSS 선언 문자열 전달 시 처리되는 구문
+		if ( typeof prop === 'string' && prop.indexOf(':') > -1 ) {
+			el.style.cssText = prop;
+		}
+		// prop 전달인자 값이 객체(CSS Map)일 때 처리되는 구문
+		if ( _isObject(prop) ) {
+			_assignObjectAttributes(el, prop);
+		}
+		// 값을 가져오거나, 설정(단일 속성)하는 코드
+		if (!value) {
+			return _getStyle(el, prop);
+		} else {
+			_setStyle(el, prop, value);
+		}
+	}
+
+
+	/**
+	 * --------------------------------
+	 * 반환되는 DOM 객체 (공개)
+	 * --------------------------------
+	 */
 	return {
-		'attr'    : _attr,
-		'prepend' : _prepend,
-		'append'  : _append,
-		'before'  : _before,
-		'after'   : _after
+
+		'isObject'    : _isObject,
+		'isArray'     : _isArray,
+		'isFunction'  : _isFunction,
+
+		'attr'        : _attr,
+		'css'         : _css,
+
+		'prependHTML' : _prependHTML,
+		'appendHTML'  : _appendHTML,
+		'beforeHTML'  : _beforeHTML,
+		'afterHTML'   : _afterHTML,
+
+		'firstChild'  : _firstChild,
+		'lastChild'   : _lastChild,
+		'append'      : _append,
+		'prepend'     : _prepend,
+		'before'      : _before
+
 	};
 
 })(window);
