4c39d80c7e980e1a551cc569fa1226ed83afa4f6
Jee Hoon <yamoo9@naver.com>
2015-11-28 21:56:37 +0900

    8일차 수업 정리

---
+++ DAY08/RequireJS-DEMO/build/build.css.config.js
@@ -0,0 +1,19 @@
+/* build.css.config.js */
+({
+	cssIn : "../css/style.css",
+	out   : "../css/style.min.css",
+	/**
+	 * --------------------------------
+	 * 사용 가능한 옵션
+	 * https://github.com/jrburke/r.js/blob/master/build/example.build.js#L218
+	 * --------------------------------
+	 * 모두 압축: ""
+	 * 라인 유지: "standard.keepLines"
+	 * 주석 유지: "standard.keepComments"
+	 * 라인/주석 유지: "standard.keepComments.keepLines"
+	 * 공백 유지: "standard.keepWhitespace"
+	 * --------------------------------
+	 */
+	optimizeCss: ""
+
+})
--- DAY08/RequireJS-DEMO/js/main.js
+++ DAY08/RequireJS-DEMO/js/main.js
@@ -4,14 +4,31 @@ require.config({
 	'baseUrl': 'js/',
 
 	'paths': {
-		'Counter':   'modules/module-Counter',
-		'Type':      'modules/module-Type',
-		'Events':    'modules/module-Events',
-		'DOM':       'modules/module-DOM',
-		'DOM.css':   'modules/module-DOM.css',
-		'jquery':    'vender/jquery-2.1.4.min',
-		'modernizr': 'vender/modernizr-custom.min'
+		'Counter':       'modules/module-Counter',
+		'Type':          'modules/module-Type',
+		'Events':        'modules/module-Events',
+		'DOM':           'modules/module-DOM',
+		'DOM.css':       'modules/module-DOM.css',
+		'jquery':        'vender/jquery-2.1.4.min',
+		'modernizr':     'vender/modernizr-custom.min',
+		'detectizr':     'vender/detectizr',
+		// 'backbone':   'vender/backbone',
+		// 'underscore': 'vender/underscore'
 	},
+
+	'shim': {
+		'modernizr': {
+			'exports': 'Modernizr'
+		},
+		'detectizr': {
+			'exports': 'Detectizr',
+			'deps':    ['modernizr']
+		}
+		// 'backbone': {
+		// 	'exports': 'Backbone',
+		// 	'deps': ['jquery', 'underscore']
+		// }
+	}
 
 	// 'urlArgs': 'ts=' + (new Date()).getTime()
 
@@ -20,13 +37,34 @@ require.config({
 
 // 모듈 정의(define), 사용(require)
 // ---------------------------------
-
 require([
 	'jquery',
+	'detectizr'
+	// 'backbone'
+	// 'modernizr'
 	// 'DOM.css'
-], function($) {
+], function( $, Detectizr ) {
 	'use strict';
 
-	console.log( $().jquery );
+	var $section = $('<section role="main">')
+			.html('<article>야무 한글 로렘입숨 로렘입숨이란? 디자인을 위한 더미 텍스트!. 하고 거추장스런, 흐르는 해도 꽃처럼 쉬지.</article>');
+
+	var $body = $('body').attr({
+		'id':   'dynamic-assing-id',
+		'lang': 'ko-KR'
+	}).append( $section );
+
+	if ( Modernizr.es5array ) {
+		var $supportES5 = $('<p>', {
+			'class': 'support-es5',
+			'text':  'ECMA Script v5를 지원하는 클라이언(브라우저) 환경입니다.'
+		}).prependTo( $body );
+	}
+
+	// console.log( Backbone );
+
+	// console.log( Modernizr );
+
+	// console.log( $().jquery );
 
 });
