ca23b3c4e9fee2d4094fa332512510b86287672c
Jee Hoon <yamoo9@naver.com>
2015-12-17 18:56:36 +0900

    13일차 내용 추가

---
+++ DAY13/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	// 파일 경로
+	'paths': {
+		'html': './src/**/*.html',
+		'sass': {
+			'src': './src/sass/**/*.{sass,scss}',
+			'dest': './src/css'
+		}
+	},
+	// 옵션
+	'options' : {
+		'sass': {
+			'outputStyle': 'expanded'
+		},
+		'browserSync': {
+			'server': './src'
+		}
+	}
+};
